a8d1688332c9abdc251623e91f34790fbea727af
Julio Polistschuk <juliopolistschukw@gmail.com>
2022-06-11 19:20:04 -0300

    .

---
+++ ExoApi/Contexts/ExoApiContext.cs
@@ -0,0 +1,28 @@
+﻿using ExoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExoApi.Contexts
+{
+    public class ExoApiContext : DbContext
+    {
+        public ExoApiContext()
+        {
+
+        }
+        public ExoApiContext(DbContextOptions<ExoApiContext> options) : base(options)
+        {
+
+        }
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=PGL-PC;Initial Catalog=ExoApiDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
---
+++ ExoApi/Program.cs
@@ -0,0 +1,68 @@
+using ExoApi.Contexts;
+using ExoApi.Interfaces;
+using ExoApi.Repositories;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultChallengeScheme = "JwtBearer";
+    options.DefaultAuthenticateScheme = "JwtBearer";
+
+}).AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("Exoapi-chave-autenticacao")),
+        ClockSkew = TimeSpan.FromMinutes(60),
+        ValidIssuer = "ExoAPI.webapi",
+        ValidAudience = "ExoAPI.webapi"
+    };
+});
+
+builder.Services.AddScoped<ExoApiContext, ExoApiContext>();
+
+builder.Services.AddTransient<IProjetoRepository, ProjetoRepository>();
+
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
